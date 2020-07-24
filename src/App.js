d52a951a564ea215a78a3a86746ed8e56da4ac5a
Oluwatosin Jimoh <57687166+ekiira@users.noreply.github.com>
2020-07-24 15:23:34 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ const [query, setQuery] = useState('chicken');
 // fetch the data from the api
 useEffect ( () => {
   getRecipes();
-}, [query]);
+}, [query, getRecipes]);
 
 const getRecipes = async () => {
   const response =  await fetch( `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`)
